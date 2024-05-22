a4a4d88ebfc4db1fc436183fe454cb07236b2e7c
('3') <mozisan@users.noreply.github.com>
2024-05-22 21:08:03 +0900

    feat(eslint-plugin): introduce `eslint-plugin-security`

--- packages/eslint-plugin/src/bases/base.ts
+++ packages/eslint-plugin/src/bases/base.ts
@@ -1,7 +1,13 @@
 import type { TSESLint } from "@typescript-eslint/utils";
 
 export default {
-	extends: ["eslint:recommended", "plugin:import-x/recommended", "plugin:unicorn/recommended", "prettier"],
+	extends: [
+		"eslint:recommended",
+		"plugin:security/recommended-legacy",
+		"plugin:import-x/recommended",
+		"plugin:unicorn/recommended",
+		"prettier",
+	],
 	plugins: ["perfectionist", "unused-imports"],
 	rules: {
 		// eslint
